f344b30d684242b640dd25102733e00bcff07242
AARON-WISDOM001 <aaronwisdom43@gmail.com>
2025-10-26 16:41:40 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/https://github.com/AARON-WISDOM001/ticketApp-hng.git/'
   plugins: [react()],
 
   // This tells Vite to process CSS and automatically
